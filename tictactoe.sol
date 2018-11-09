4d0f671b46f08739a407228ec4b58147a1db385c
Yaghyavardhan singh khangarot <yaghya.jaipur@gmail.com>
2018-11-09 10:42:39 +0530

    Added Move Function

--- tictactoe.sol
+++ tictactoe.sol
@@ -14,7 +14,38 @@ contract tictactoe{
 		p2 = msg.sender;
 	}
 
-	function DoMove(uint){
-	
+
+	function DoMove(uint place) returns (string){
+        
+       		uint winner = Status();
+
+        	if(winner == 1){
+            		return "Winner is X";
+        	}
+       		if (winner == 2){
+           		return " Winner is O";
+        	}
+        	if(start == 0){
+            		if(msg.sender != p1) return "player 1's Turn";
+        	}else if(start == 1){
+            		if(msg.sender != p2) return "player 2's Turn";
+        	}
+        
+        	if(place < 0 || place >= 9) return "Invalid Number  Enter number [0,9)";
+        
+        	if(board[place] != 0) return "already occupied";
+        
+        	board[place] = start+1;
+        	start = 1- start;
+        
+        
+        	return "Works Fine";   
+    	}
+
+
+	function Status() returns(uint){
+		
+		
 	}
+
 }
